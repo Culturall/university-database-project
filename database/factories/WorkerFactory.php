28e9976789360a7f1df38df82b5f707dc8517356
kristiannotari <kristiannotari2014@gmail.com>
2018-02-20 00:05:19 +0100

    started models, factory, seedings

---
+++ database/factories/WorkerFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Worker::class, function (Faker $faker) {
+    return [
+        'name' => $faker->firstName,
+        'surname' => $faker->lastName,
+        'password' => $faker->md5,
+        'birthdate' => $faker->date,
+        'email' => $faker->unique()->email,
+        'requester' => $faker->boolean
+    ];
+});
