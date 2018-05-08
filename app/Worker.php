2799e318f22852e645756d2a0d7340d491c6175d
kristiannotari <kristian.notari@studenti.unimi.it>
2018-05-08 17:37:54 +0200

    added working auth

--- app/Worker.php
+++ app/Worker.php
@@ -5,9 +5,12 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class Worker extends Authenticatable
 {
     use Notifiable;
+
+    protected $table = 'worker';
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'surname', 'birthdate', 'email', 'password',
     ];
 
     /**
